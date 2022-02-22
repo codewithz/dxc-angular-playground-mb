3477849c951a90468cc71a757c161f8ccccca9e4
Zartab Nakhwa <zartab@codewithz.com>
2022-02-22 11:54:07 +0530

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyComponent } from './mycomponent.component';
+import { CourseComponent } from './course/course.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyComponent,
+    CourseComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
