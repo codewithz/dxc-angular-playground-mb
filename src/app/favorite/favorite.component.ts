32ef305ead42009af7935d2a01c6cf70ea72c281
Zartab Nakhwa <zartab@codewithz.com>
2022-02-24 10:28:22 +0530

    Input Way-2

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -3,11 +3,13 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
+  inputs: ['isSelected']
 })
 export class FavoriteComponent implements OnInit {
 
-  @Input() isSelected: boolean = true;
+  // @Input() isSelected: boolean = true;
+  isSelected: boolean = true;
 
   constructor() { }
 
