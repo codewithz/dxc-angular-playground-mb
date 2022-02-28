bc33009fff70440f1907924aa4ff8198a39d4eb4
Zartab Nakhwa <zartab@codewithz.com>
2022-02-28 12:17:29 +0530

    Http Calls- Post

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -9,16 +9,32 @@ import { HttpClient } from '@angular/common/http';
 export class PostsComponent implements OnInit {
 
   posts: any = [];
+  private url: string = 'https://jsonplaceholder.typicode.com/posts';
 
   constructor(private http: HttpClient) {
 
-    http.get('https://jsonplaceholder.typicode.com/posts')
+    http.get(this.url)
       .subscribe(
         (response) => {
           this.posts = response;
         }
       )
 
+  }
+
+  createPost(input: HTMLInputElement) {
+    // console.log(input.value)
+    let post = { title: input.value, id: 0 }
+    this.http
+    .post<{ title?: string, id?: number }>(this.url, JSON.stringify(post))
+      .subscribe(
+        (response) => {
+          let id = response.id ? response.id : 0
+          post.id = id;
+          console.log(post)
+          this.posts.splice(0, 0, post)
+        }
+      )
   }
 
   ngOnInit(): void {
