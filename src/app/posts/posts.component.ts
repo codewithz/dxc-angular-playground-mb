00025978388597828522868586483a9267374f98
Zartab Nakhwa <zartab@codewithz.com>
2022-02-28 12:23:29 +0530

    Http Call- Put

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface Post {
+  title?: string,
+  id?: number
+}
+
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
@@ -26,13 +31,23 @@ export class PostsComponent implements OnInit {
     // console.log(input.value)
     let post = { title: input.value, id: 0 }
     this.http
-    .post<{ title?: string, id?: number }>(this.url, JSON.stringify(post))
+      .post<{ title?: string, id?: number }>(this.url, JSON.stringify(post))
       .subscribe(
         (response) => {
           let id = response.id ? response.id : 0
           post.id = id;
           console.log(post)
           this.posts.splice(0, 0, post)
+        }
+      )
+  }
+
+  updatePost(post: Post) {
+    post.title = 'Updated:' + post.title;
+    this.http.put(this.url + '/' + post.id, post)
+      .subscribe(
+        (response) => {
+          console.log(response)
         }
       )
   }
